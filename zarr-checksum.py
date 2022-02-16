a24e1cb9a2560c35fe410b6162360d5668413e3b
John T. Wodder II <git@varonathe.org>
2022-02-16 16:21:41 -0500

    A slight variation on the threaded implementation

--- zarr-checksum.py
+++ zarr-checksum.py
@@ -11,6 +11,7 @@ from dataclasses import dataclass, field
 from hashlib import md5
 import logging
 import os
+import os.path
 from pathlib import Path
 import threading
 from typing import Dict, Iterable, Tuple, Union
@@ -109,11 +110,9 @@ def threaded_walk(dirpath: Path, threads=DEFAULT_THREADS) -> Iterable[Tuple[Path
                     path = paths.pop()
                     break
             try:
-                dirs = []
                 for item in os.listdir(path):
                     subpath = os.path.join(path, item)
                     if os.path.isdir(subpath):
-                        dirs.append(item)
                         with lock:
                             tasks += 1
                             paths.append(subpath)
@@ -148,9 +147,72 @@ def threaded_walk(dirpath: Path, threads=DEFAULT_THREADS) -> Iterable[Tuple[Path
             threads -= 1
 
 
+def threaded_walk2(dirpath: Path, threads=DEFAULT_THREADS) -> Iterable[Tuple[Path, str]]:
+    if not os.path.isdir(dirpath):
+        return
+    lock = threading.Lock()
+    on_input = threading.Condition(lock)
+    on_output = threading.Condition(lock)
+    tasks = 1
+    paths = [dirpath]
+    output = []
+
+    def worker():
+        nonlocal tasks
+        while True:
+            with lock:
+                while True:
+                    if not tasks:
+                        output.append(None)
+                        on_output.notify()
+                        return
+                    if not paths:
+                        on_input.wait()
+                        continue
+                    path = paths.pop()
+                    break
+            try:
+                if os.path.isdir(path):
+                    for item in os.listdir(path):
+                        with lock:
+                            tasks += 1
+                            paths.append(os.path.join(path, item))
+                            on_input.notify()
+                else:
+                    digest = md5digest(path)
+                    with lock:
+                        output.append((Path(path), digest))
+                        on_output.notify()
+            except OSError:
+                log.exception("Error digesting path %s", path)
+            finally:
+                with lock:
+                    tasks -= 1
+                    if not tasks:
+                        on_input.notify_all()
+
+    workers = [
+        threading.Thread(target=worker, name=f"fastio.walk {i} {dirpath}", daemon=True)
+        for i in range(threads)
+    ]
+    for w in workers:
+        w.start()
+    while threads or output:  # TODO(jart): Why is 'or output' necessary?
+        with lock:
+            while not output:
+                on_output.wait()
+            item = output.pop()
+        if item:
+            yield item
+        else:
+            threads -= 1
+
+
+
 WALKERS = {
     "sync": sync_walk,
     "threads": threaded_walk,
+    "threads2": threaded_walk2,
 }
 
 
