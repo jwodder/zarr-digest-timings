f7ebee0d498ee24e1fd3066e2609eb8501ad3db0
John T. Wodder II <git@varonathe.org>
2022-02-16 16:16:34 -0500

    Threaded walk

--- zarr-checksum.py
+++ zarr-checksum.py
@@ -5,15 +5,23 @@ __requires__ = [
     "click >= 8.0",
     "dandischema >= 0.5.1",
 ]
+
 from collections import deque
 from dataclasses import dataclass, field
 from hashlib import md5
+import logging
+import os
 from pathlib import Path
+import threading
 from typing import Dict, Iterable, Tuple, Union
 import click
 from dandischema.digests.zarr import get_checksum
 
 DIGEST_BLOCK_SIZE = 1 << 16
+
+DEFAULT_THREADS = min(32, (os.cpu_count() or 1) + 4)
+
+log = logging.getLogger()
 
 
 @dataclass
@@ -76,12 +84,83 @@ def sync_walk(dirpath: Path) -> Iterable[Tuple[Path, str]]:
                 yield (p, md5digest(p))
 
 
+def threaded_walk(dirpath: Path, threads=DEFAULT_THREADS) -> Iterable[Tuple[Path, str]]:
+    if not os.path.isdir(dirpath):
+        return
+    lock = threading.Lock()
+    on_input = threading.Condition(lock)
+    on_output = threading.Condition(lock)
+    tasks = 1
+    paths = [dirpath]
+    output = []
+
+    def worker():
+        nonlocal tasks
+        while True:
+            with lock:
+                while True:
+                    if not tasks:
+                        output.append(None)
+                        on_output.notify()
+                        return
+                    if not paths:
+                        on_input.wait()
+                        continue
+                    path = paths.pop()
+                    break
+            try:
+                dirs = []
+                for item in os.listdir(path):
+                    subpath = os.path.join(path, item)
+                    if os.path.isdir(subpath):
+                        dirs.append(item)
+                        with lock:
+                            tasks += 1
+                            paths.append(subpath)
+                            on_input.notify()
+                    else:
+                        digest = md5digest(subpath)
+                        with lock:
+                            output.append((Path(subpath), digest))
+                            on_output.notify()
+            except OSError:
+                log.exception("Error scanning directory %s", path)
+            finally:
+                with lock:
+                    tasks -= 1
+                    if not tasks:
+                        on_input.notify_all()
+
+    workers = [
+        threading.Thread(target=worker, name=f"fastio.walk {i} {dirpath}", daemon=True)
+        for i in range(threads)
+    ]
+    for w in workers:
+        w.start()
+    while threads or output:  # TODO(jart): Why is 'or output' necessary?
+        with lock:
+            while not output:
+                on_output.wait()
+            item = output.pop()
+        if item:
+            yield item
+        else:
+            threads -= 1
+
+
+WALKERS = {
+    "sync": sync_walk,
+    "threads": threaded_walk,
+}
+
+
 @click.command()
 @click.argument(
     "dirpath", type=click.Path(exists=True, file_okay=False, path_type=Path)
 )
-def main(dirpath: Path) -> None:
-    print(checksum_file_list(dirpath, sync_walk(dirpath)))
+@click.argument("implementation", type=click.Choice(list(WALKERS)))
+def main(dirpath: Path, implementation: str) -> None:
+    print(checksum_file_list(dirpath, WALKERS[implementation](dirpath)))
 
 
 if __name__ == "__main__":
