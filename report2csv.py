c3b680a19d09c6c8c0494d616f9f82a8d8bcbd07
John T. Wodder II <git@varonathe.org>
2022-02-17 16:15:59 -0500

    Script for converting JSON reports to CSV

---
+++ report2csv.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import argparse
+import csv
+import json
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(
+        description="Convert a file of JSON Lines zarr-digest-timings reports to CSV"
+    )
+    parser.add_argument("-o", "--outfile", type=argparse.FileType("w"), default="-")
+    parser.add_argument("report", type=argparse.FileType("r"))
+    args = parser.parse_args()
+    with args.outfile:
+        out = csv.DictWriter(
+            args.outfile,
+            [
+                "dirpath",
+                "implementation",
+                "threaded_fscacher",
+                "caching",
+                "caching_files",
+                "threads",
+                "number",
+                "avgtime",
+            ],
+        )
+        out.writeheader()
+        with args.report:
+            for line in args.report:
+                out.writerow(json.loads(line))
+
+
+if __name__ == "__main__":
+    main()
