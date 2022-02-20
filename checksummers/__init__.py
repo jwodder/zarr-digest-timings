473e2787ae09643a65c365614d1081704bf97cb2
John T. Wodder II <git@varonathe.org>
2022-02-20 00:46:16 -0500

    Another trio variant that's only slightly faster

--- checksummers/__init__.py
+++ checksummers/__init__.py
@@ -2,15 +2,16 @@ from .fastio import FastioWalker
 from .oothreads import OOThreadsWalker
 from .recursive import RecursiveChecksummer
 from .sync import SyncWalker
-from .trio import TrioWalker
+from .trio import TrioLiveWalker, TrioWalker
 
 CLASSES = {
     "sync": SyncWalker,
     "fastio": FastioWalker,
     "oothreads": OOThreadsWalker,
     "trio": TrioWalker,
+    "trio-live": TrioLiveWalker,
     "recursive": RecursiveChecksummer,
 }
 
 # Classes that are affected by the `threads` parameter
-THREADED_CLASSES = {"fastio", "oothreads", "trio"}
+THREADED_CLASSES = {"fastio", "oothreads", "trio", "trio-live"}
--- checksummers/trio.py
+++ checksummers/trio.py
@@ -17,7 +17,12 @@ from typing import (
     Union,
 )
 import trio
-from .bases import DIGEST_BLOCK_SIZE, IterativeChecksummer
+from .bases import (
+    DIGEST_BLOCK_SIZE,
+    IterativeChecksummer,
+    ZarrChecksumCompiler,
+    ZarrChecksummer,
+)
 
 T = TypeVar("T")
 
@@ -48,6 +53,52 @@ class TrioWalker(IterativeChecksummer):
                 async for f in receiver:
                     files.append(f)
         return files
+
+    async def async_worker(
+        self,
+        jobs: AsyncJobStack[Path],
+        sender: trio.MemorySendChannel[Tuple[Path, str]],
+    ) -> None:
+        async with sender, aclosing(aiter(jobs)) as jobiter:
+            async for ctx in jobiter:
+                async with ctx as dirpath:
+                    for p in dirpath.iterdir():
+                        if p.is_dir():
+                            await jobs.put(p)
+                        else:
+                            dgst = await self.async_md5digest(p)
+                            await sender.send((p, dgst))
+
+    @staticmethod
+    async def async_md5digest(filepath: Path) -> None:
+        dgst = md5()
+        async with await trio.open_file(filepath, "rb") as fp:
+            while True:
+                blob = await fp.read(DIGEST_BLOCK_SIZE)
+                if not blob:
+                    break
+                dgst.update(blob)
+        return dgst.hexdigest()
+
+
+class TrioLiveWalker(ZarrChecksummer):
+    def checksum(self, dirpath: Union[str, Path]) -> str:
+        return trio.run(self.async_checksum, Path(dirpath))
+
+    async def async_checksum(self, dirpath: Path) -> str:
+        zcc = ZarrChecksumCompiler(dirpath)
+        jobs = AsyncJobStack([dirpath])
+        async with trio.open_nursery() as nursery:
+            sender, receiver = trio.open_memory_channel(0)
+            # We need to limit the number of workers or else we get a "Too many
+            # open files" error
+            async with sender:
+                for _ in range(self.threads):
+                    nursery.start_soon(self.async_worker, jobs, sender.clone())
+            async with receiver:
+                async for f, dgst in receiver:
+                    zcc.add(f, dgst)
+        return zcc.get_digest()
 
     async def async_worker(
         self,
